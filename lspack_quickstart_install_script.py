5ed4b2617ce979504ac2e148bfcbe20afc65c902
Leander Lombardi <leanderlombardi@gmail.com>
2024-02-11 18:32:16 +0100

    minor chaneg

--- lspack_quickstart_install_script.py
+++ lspack_quickstart_install_script.py
@@ -2,8 +2,8 @@ import subprocess
 packdir = input("Enter package directory (full name; /home/leander/...): ")
 files = input("Enter .c files: (full name; /home/leander/..., separated by comma): ").split(",")
 packname = input("Enter package name: ")
-subprocess.run([f"echo \"sudo gcc -o \\\"/usr/bin/{packname}\\\" ",end=""], shell=True)
+subprocess.run([f"echo \"sudo gcc -o \\\"/usr/bin/{packname}\\\" \\"], shell=True)
 for f in files:
-    subprocess.run([f"{f} ",end=""], shell=True)
+    subprocess.run([f"{f} \\"], shell=True)
 subprocess.run([f"\\\">> {packdir}/lspack_install_script.sh"])
 print("Successfully generated lspack_install_script.sh!")
